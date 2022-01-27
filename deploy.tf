9299c09dd3fcb5e824163e6469319d95d3816173
valerio <vcrini@gmail.com>
2022-01-27 16:04:12 +0100

    initial commit

---
+++ deploy.tf
@@ -0,0 +1,92 @@
+module "ecr_immutable" {
+  image_tag_mutability = "IMMUTABLE"
+  name                 = local.ecr_repositories
+  policy               = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep no more than 5 images ",
+            "selection": {
+                "countNumber": 5,
+                "countType": "imageCountMoreThan",
+                "tagStatus": "any"
+            },
+            "action": {
+                "type": "expire"
+            }
+        }    
+      ]
+}
+EOF
+  #source = "/Users/vcrini/Repositories/terraform-modules/ecr"
+  source = "git::https://bitbucket.org/valeri0/ecr.git?ref=0.2.0"
+  tags   = var.tag
+}
+module "ecr_mutable" {
+  name   = formatlist("%s-%s", local.ecr_repositories, "snapshot")
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 3 images",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 3
+            },
+            "action": {
+                "type": "expire"
+            }
+        }    
+      ]
+}
+EOF
+  source = "git::https://bitbucket.org/valeri0/ecr.git?ref=0.2.0"
+  tags   = var.tag
+}
+module "deploy" {
+  # source                  = "/Users/vcrini/Repositories/terraform-modules/deploy_x_application"
+  branch_name             = var.branch_name
+  buildspec               = local.buildspec
+  cluster_name            = var.aws_ecs_cluster
+  codepipeline_bucket     = var.codepipeline_bucket
+  deploy_environment      = var.deploy_environment
+  deployspec              = local.deployspec
+  image                   = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+  poll_for_source_changes = "false"
+  repository_name         = local.repository_name
+  role_arn                = local.role_arn
+  role_arn_codebuild      = local.role_arn_codebuild
+  role_arn_codepipeline   = local.role_arn_codepipeline
+  role_arn_source         = local.role_arn_source
+  s3_cache                = var.s3_cache
+  #source                  = "/Users/vcrini/Repositories/terraform-modules/deploy_x_application"
+  source = "git::https://bitbucket.org/valeri0/deploy_x_application?ref=0.8.0"
+  tags   = var.tag
+}
+
+resource "aws_cloudwatch_log_group" "log" {
+  for_each          = toset([module.deploy.cloudwatch_build_log, module.deploy.cloudwatch_deploy_log])
+  name              = each.value
+  retention_in_days = var.retention_in_days
+  tags              = var.tag
+}
+
+module "balancer" {
+  alarm_arn            = var.alarm_arn
+  default_cname        = var.default_cname
+  repository_name      = local.repository_name
+  deregistration_delay = 120
+  listener             = var.listener
+  lb_name              = var.lb_name
+  nlb_name             = var.nlb_name
+  source = "/Users/vcrini/Repositories/terraform-modules//load_balancer"
+  #source              = "git::https://bitbucket.org/valeri0/load_balancer.git//?ref=0.6.0"
+  ssl_certificate_arn = local.ssl_certificate_arn
+  tags                = var.tag
+  target_group        = var.target_group
+  target_group_prefix = var.target_group_prefix
+  vpc_id              = var.vpc_id
+}
---
+++ variables_balancer.tf
@@ -0,0 +1,44 @@
+
+variable "alarm_arn" {
+  type        = string
+  description = "SNS topic to alert when balancer fails"
+}
+variable "default_cname" {
+  type        = string
+  description = "cname to use if not specified insied target_groups"
+}
+
+variable "lb_name" {
+  description = "balancer name"
+  type        = string
+}
+variable "nlb_name" {
+  default     = null
+  type        = string
+  description = "name of LB"
+}
+
+variable "listener" {
+  description = "map representing listener configuration"
+  type        = map(any)
+}
+variable "ssl_certificate_arn_name" {
+  default     = ""
+  description = "ssl certificate used by  listener if HTTPS"
+  type        = string
+}
+variable "target_group" {
+  description = "map representing target group configuration"
+  type        = map(any)
+}
+variable "target_group_prefix" {
+  description = "short string used to differentiate each target group prefix"
+  type        = string
+}
+variable "vpc_id" {
+  description = "id representing AWS Virtual Private Cloud"
+  type        = string
+}
+locals {
+  ssl_certificate_arn = "arn:aws:iam::${local.account_id}:${var.ssl_certificate_arn_name}"
+}
